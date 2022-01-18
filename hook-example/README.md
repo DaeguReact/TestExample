944e42eac01213e4e449d8db5a6198ac4744eb24
하욱현(ukhyun.ha) <ukhyun.ha@slworld.com>
2022-01-18 14:54:30 +0900

    hook sample 업로드

--- hook-example/README.md
+++ hook-example/README.md
@@ -3,3 +3,11 @@
 1. InputBox의 내용을 그대로 출력하는 앱
 
 ![샘플이미지](sample/hook-example(input).gif)
+
+2. InputBox의 내용을 받아 리스트로 출력하는 앱
+
+![샘플이미지](sample/hook-example(list).gif)
+
+2-1. (심화) InputBox를 내용을 Enter로도 입력할 수 있도록, 색 이름을 입력했을 시 그 색으로 출력되는 앱
+
+![샘플이미지](sample/hook-example(list-advance-1).gif)
--- hook-example/src/App.js
+++ hook-example/src/App.js
@@ -7,13 +7,31 @@ function App() {
   const handlingChange = (e) => {
     setText(e.target.value);
   }
+
+  const [items, setItems] = useState([]);
+
+  const hanldeButtonClick = () => {
+    setItems([...items, text]);
+    setText('');
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      hanldeButtonClick();
+    }
+  }
+
   return (
     <div id="app2">
-      <input type="text" onChange={handlingChange} />
-      <div>{text}</div>
+      <div id="input-wrap">
+        <input type="text" onChange={handlingChange} value={text} onKeyDown={handleKeyDown} />
+        <button onClick={hanldeButtonClick}>추가하기</button>
+      </div>
+      <ul>
+        {items.map((item, index) => <li style={{ 'color': item }} key={index}>{item}</li>)}
+      </ul>
     </div>
 
   );
 }
-
 export default App;
